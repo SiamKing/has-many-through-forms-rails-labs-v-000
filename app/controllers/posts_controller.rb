7ad9423bb290335cc5ec3bac64842ef179c952d0
SiamKing <thesiameseelectric@gmail.com>
2018-12-02 19:01:42 -0800

    Postit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+      @comment = Comment.new
   end
 
   def index
@@ -12,8 +13,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Great success"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = "There was an error"
+      render :new
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,18 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :categories, through: :post_categories, autosave: true
   has_many :comments
   has_many :users, through: :comments
+  
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |cat_attribute|
+      category = Category.find_or_create_by(cat_attribute)
+      self.categories << category
+    end
+  end
 
-
+  def unique_users
+    self.users.uniq
+  end
+  
 end
